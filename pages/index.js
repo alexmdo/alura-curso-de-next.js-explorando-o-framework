6c6e8b812fd2c2049fc367f455ada10fd80f80fb
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-20 16:25:59 -0300

    Componentes do layout

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
-import Link from "../src/components/Link"
+import HomeScreen from '../src/screens/HomeScreen';
 
-function Title({ children, as }) {
-    const Tag = as;
-    return (
-        <>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                }
-            `}</style>
-        </>
+export default HomeScreen;
 
-    );
-}
+// import Link from "../src/components/Link"
+// import Footer from "../src/components/Footer"
 
-export default function HomePage() {
-    return (
-        <div>
-            <Title as="h1">Alura Cases - Home</Title>
-            <Link href="/faq">
-                Ir para o FAQ
-            </Link>
-        </div>
-    )
-}
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </>
+
+//     );
+// }
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as="h1">Alura Cases - Home</Title>
+//             <Link href="/faq">
+//                 Ir para o FAQ
+//             </Link>
+//             <Footer />
+//         </div>
+//     )
+// }
