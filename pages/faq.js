2481b4d6e35aff2990b7fc49391786e6f8543e01
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-26 17:11:14 -0300

    Encapsulamento do title

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
 import Link from "../src/components/Link"
+import PageTitle from "../src/components/PageTitle"
 
 export const getStaticProps = async context => {
     const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
@@ -21,9 +21,7 @@ export const getStaticProps = async context => {
 export default function FAQPage({ faq }) {
     return (
         <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
+            <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
             <h1>Alura Cases - Pagina de Perguntas FAQ</h1>
             <Link href="/">
                 Ir para a home
